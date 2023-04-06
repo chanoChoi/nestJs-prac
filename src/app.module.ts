fd7d852a8495acf5ba7b788a1b4641f62c5d98f2
Chaho Choi <dhfkdhaneod@gmail.com>
2023-04-06 23:24:17 +0900

    [Test] AuthService e2e test and change the pipe & session craetes at the app.module AND add cross-env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,60 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
 import { User } from './user/user.entitiy';
 import { Report } from './report/report.entity';
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite', // file 기반의 DB
-      database: 'db.sqlite', // root paht에 해당 이름의 DB 파일 생성 
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite', // file 기반의 DB
+    //   database: 'db.sqlite', // root paht에 해당 이름의 DB 파일 생성 
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
     UserModule,
     ReportModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['asdfqwdasd'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['asdasfaaafaa'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // 미리 선언한 프로퍼티외의 추가 프로퍼티는 제거
-    }),
-  );
+  // app.use(
+  //   cookieSession({
+  //     keys: ['asdasfaaafaa'],
+  //   }),
+  // );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true, // 미리 선언한 프로퍼티외의 추가 프로퍼티는 제거
+  //   }),
+  // );  main.ts 에서 설정
   await app.listen(3000);
 }
 bootstrap();
