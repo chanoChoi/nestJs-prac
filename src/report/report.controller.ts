8f2e4af9e9482d63895b13b1dc3e4310a24829f7
Chaho Choi <dhfkdhaneod@gmail.com>
2023-04-07 11:57:33 +0900

    [Feat] Report Contoller, Service, Repository

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportService } from './report.service';
+import { AuthGuard } from 'src/guard/auth.guard';
 
-@Controller('report')
-export class ReportController {}
+@Controller('reports')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @UseGuards(AuthGuard)
+  @Post()
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportService.create(body);
+  }
+}
--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+  constructor(
+    @InjectRepository(Report)
+    private readonly reportRepository: Repository<Report>,
+  ) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.reportRepository.create(reportDto);
+
+    return this.reportRepository.save(report);
+  }
+}
