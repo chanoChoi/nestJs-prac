f2b1a62563c889da78ca978d23b1a6c28c6df1d5
Chaho Choi <dhfkdhaneod@gmail.com>
2023-04-07 15:20:16 +0900

    [Feat] using QueryBuilder

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { User } from 'src/user/user.entitiy';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportService {
@@ -26,5 +27,21 @@ export class ReportService {
 
     report.approved = approved;
     return this.reportRepository.save(report);
+  }
+
+  createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+    return this.reportRepository
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameter('mileage', mileage)
+      .limit(3)
+      .getRawOne();
   }
 }
