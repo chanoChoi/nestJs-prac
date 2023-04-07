32284aadcab17d729c2ccf1cac53bc0d2ce22f28
Chaho Choi <dhfkdhaneod@gmail.com>
2023-04-07 14:55:38 +0900

    [Feat] add GetEstimateDto

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -5,6 +5,8 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportService } from './report.service';
@@ -15,6 +17,7 @@ import { Serialize } from 'src/interceptor/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from 'src/guard/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportController {
@@ -31,5 +34,10 @@ export class ReportController {
   @Patch('/:id')
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportService.changeApproval(parseInt(id), body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
   }
 }
--- src/user/middleware/current-user.middleware.ts
+++ src/user/middleware/current-user.middleware.ts
@@ -1,6 +1,16 @@
 import { NestMiddleware, Injectable } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UserService } from '../user.service';
+import { User } from '../user.entitiy';
+
+//  기존 인터페이스에 속성을 추가 및 업데이트
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -10,7 +20,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
     const { userId } = req.session || {};
     if (userId) {
       const user = await this.userService.findOne(parseInt(userId));
-      // @ts-ignore
       req.currentUser = user;
     }
 
